1c6de869fdc639f27851cdaa5da2601067a28775
Gabriel Arreco <82686164+gverdan@users.noreply.github.com>
2021-06-20 16:02:23 +0000

    Corrigido a parte de interação com usuário, havia trocado o sentido dos simbolos maior e menor do Cin e Cout

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <investimentos.h>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  tInvestimento i;
+  double capital, taxaDejuros;
+  int tempo;
+
+  cout << "\nInforme o capital a ser investido = R$ ";
+  cin >> i.montanteInvestido;
+  cout << "Informe a taxa de juros %a.a. = ";
+  cin >> i.taxaDejuros;
+  cout << "Informe o periodo do investimento (em meses) = ";
+  cin >> i.periodo;
+
+
 }
